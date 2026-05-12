f08c677b5ebb53aba6cae61ea3dbc1168d7d67bd
Gopmyc <hoyaux.gaspard@gmail.com>
2026-05-12 16:51:46 +0200

    fix(player-crouch): keep visual mesh above ground while changing capsule height

--- Assets/Scripts/Player/Movement/Crouch.lua
+++ Assets/Scripts/Player/Movement/Crouch.lua
@@ -2,30 +2,39 @@
 local Crouch	=
 {
 	CROUCH_HEIGHT_MULTIPLIER	= 0.55,
-	CROUCH_SPEED_MULTIPLIER		= 0.55,
 	CROUCH_CHECK_SKIN			= 0.01,
 	CROUCH_CHECK_MARGIN			= 0.02,
+	CROUCH_KEY					= Key.LEFT_CONTROL,
 
 	_private	=
 	{
-		oTransform				= nil,
-		oPhysicalCapsule		= nil,
-		bIsCrouching			= false,
-		nStandingHeight			= 0.0,
-		nCrouchingHeight		= 0.0,
-		nCurrentSpeedMultiplier	= 1.0,
+		oTransform					= nil,
+		oPhysicalCapsule			= nil,
+		oVisualRootTransform		= nil,
+		vVisualStandingLocalPosition	= nil,
+		bIsCrouching				= false,
+		nStandingHeight				= 0.0,
+		nCrouchingHeight			= 0.0,
+		sVisualRootActorName		= "Player Visual Root",
+		sModelActorName				= "Player Model",
 	}
 }
 
 function Crouch:OnAwake()
-	local oPhysicalCapsule			= self.owner:GetPhysicalCapsule()
+	local oPhysicalCapsule				= self.owner:GetPhysicalCapsule()
 
-	self								= setmetatable(self,  self.owner:GetBehaviour("Class"))
-	self._private.oTransform			= self.owner:GetTransform()
-	self._private.oPhysicalCapsule		= oPhysicalCapsule
-	self._private.nStandingHeight		= oPhysicalCapsule:GetHeight()
-	self._private.nCrouchingHeight		= self._private.nStandingHeight * self.CROUCH_HEIGHT_MULTIPLIER
-	self._private.nCurrentSpeedMultiplier	= 1.0
+	self									= setmetatable(self,  self.owner:GetBehaviour("Class"))
+
+	local oVisualRootTransform, vVisualStandingLocalPosition	= self:ResolveVisualRootTransform()
+
+	self._private.oTransform				= self.owner:GetTransform()
+	self._private.oPhysicalCapsule			= oPhysicalCapsule
+	self._private.oVisualRootTransform		= oVisualRootTransform
+	self._private.vVisualStandingLocalPosition	= vVisualStandingLocalPosition
+	self._private.nStandingHeight			= oPhysicalCapsule:GetHeight()
+	self._private.nCrouchingHeight			= self._private.nStandingHeight * self.CROUCH_HEIGHT_MULTIPLIER
+
+	self:UpdateVisualVerticalOffset(self._private.nStandingHeight)
 end
 
 function Crouch:OnFixedUpdate(nFixedDeltaTime)
@@ -33,7 +42,7 @@ function Crouch:OnFixedUpdate(nFixedDeltaTime)
 end
 
 function Crouch:HandleCrouch()
-	local bWantsCrouch	= Inputs.GetKey(Key.LEFT_CONTROL)
+	local bWantsCrouch	= Inputs.GetKey(self.CROUCH_KEY)
 	local bIsCrouching	= self._private.bIsCrouching
 
 	if bWantsCrouch and not bIsCrouching then
@@ -97,15 +106,13 @@ end
 function Crouch:EnterCrouch()
 	self:SetCapsuleHeight(self._private.nCrouchingHeight)
 
-	self._private.bIsCrouching			= true
-	self._private.nCurrentSpeedMultiplier	= self.CROUCH_SPEED_MULTIPLIER
+	self._private.bIsCrouching	= true
 end
 
 function Crouch:ExitCrouch()
 	self:SetCapsuleHeight(self._private.nStandingHeight)
 
-	self._private.bIsCrouching			= false
-	self._private.nCurrentSpeedMultiplier	= 1.0
+	self._private.bIsCrouching	= false
 end
 
 function Crouch:SetCapsuleHeight(nTargetHeight)
@@ -123,10 +130,35 @@ function Crouch:SetCapsuleHeight(nTargetHeight)
 	vPosition.y		= vPosition.y + (nHeightDelta * 0.5)
 
 	oTransform:SetWorldPosition(vPosition)
+	self:UpdateVisualVerticalOffset(nTargetHeight)
 end
 
-function Crouch:GetSpeedMultiplier()
-	return self._private.nCurrentSpeedMultiplier
+function Crouch:ResolveVisualRootTransform()
+	local sVisualRootActorName	= self._private.sVisualRootActorName
+	local sModelActorName		= self._private.sModelActorName
+	local oVisualRootActor		= self:FindActorByNameRecursive(self.owner, sVisualRootActorName)
+	local oModelActor			= self:FindActorByNameRecursive(self.owner, sModelActorName)
+	local oVisualRootTransform	= oVisualRootActor and oVisualRootActor:GetTransform() or (oModelActor and oModelActor:GetTransform() or nil)
+	local vVisualStandingLocalPosition	= oVisualRootTransform and oVisualRootTransform:GetLocalPosition() or nil
+
+	return oVisualRootTransform, vVisualStandingLocalPosition
+end
+
+function Crouch:UpdateVisualVerticalOffset(nCurrentHeight)
+	local oVisualRootTransform			= self._private.oVisualRootTransform
+	local vVisualStandingLocalPosition	= self._private.vVisualStandingLocalPosition
+
+	if not oVisualRootTransform or not vVisualStandingLocalPosition then return end
+
+	local nStandingHeight		= self._private.nStandingHeight
+	local nHalfHeightReduction	= (nStandingHeight - nCurrentHeight) * 0.5
+	local vTargetLocalPosition	= Vector3.new(
+		vVisualStandingLocalPosition.x,
+		vVisualStandingLocalPosition.y + nHalfHeightReduction,
+		vVisualStandingLocalPosition.z
+	)
+
+	oVisualRootTransform:SetLocalPosition(vTargetLocalPosition)
 end
 
 function Crouch:IsCrouching()
